761cf6c938ab73279ebad2d6f223583edc7f3733
Ivan Longarai <ivanslongarai@gmail.com>
2021-09-19 08:46:32 -0300

    first endpoint test

---
+++ src/Grocery.API/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grocery.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            return Ok("It works!!");
+        }
+    }
+}
